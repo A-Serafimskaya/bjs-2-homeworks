ed6f621b6393f5b74a1354b4d5107fd45138cf21
Alexandra Serafimskaya <a79256103066@gmail.com>
2026-02-15 17:49:39 +0300

    3. Arrays: add task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+    debugger;
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    return arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const filteredUsers = users.filter((user) => user.gender === gender);
+    if (filteredUsers.length === 0) {
+        return 0;
+    }
+    return filteredUsers.map((user) => user.age).reduce((sum, age) => sum + age, 0) / filteredUsers.length;
 }
