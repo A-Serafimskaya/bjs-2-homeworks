efd4ee0f53e4b14f3bf4d21838e79c582585d240
Alexandra Serafimskaya <a79256103066@gmail.com>
2026-02-18 13:31:38 +0300

    4. objects: add task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,44 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
+let student1 = new Student("Мария", "female", 35);
+let student2 = new Student("Василий", "male", 22);
+let student3 = new Student("Ирина", "female", 19);
+
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (!this.marks) {
+        console.log("Невозможно добавить оценки: студент отчислен.");
+        return false;
+    }
+    this.marks.push(...marks);
+    return true;
+
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+    }
+    const sum = this.marks.reduce((accumulator, currentMark) => accumulator + currentMark, 0);
+
+    return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+
+    this.excluded = reason;
+    this.reason = reason;
+
+    return true;
 }
+
